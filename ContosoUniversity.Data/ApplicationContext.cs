ebf75a63e318bec5d2aee4756ff95f31e422c4cf
Adrian Limon <me@adrianlimon.com>
2017-07-20 16:24:14 -0700

    added data access layer project to solution

---
+++ ContosoUniversity.Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ ContosoUniversity.Data/ApplicationContextFactory.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ContosoUniversity.Data
+{
+    // modeled from https://www.benday.com/2017/02/17/ef-core-migrations-without-hard-coding-a-connection-string-using-idbcontextfactory
+    public class ApplicationContextFactory : IDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext Create()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("Hosting:Environment");
+            var basePath = AppContext.BaseDirectory;
+            return Create(basePath, environmentName);
+        }
+
+        private ApplicationContext Create(string basePath, string environmentName)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("settings.json")
+                .AddJsonFile($"settings.{environmentName}.json", true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            var connstr = config.GetConnectionString("DefaultConnection");
+
+            if (String.IsNullOrWhiteSpace(connstr))
+            {
+                throw new InvalidOperationException("Could not find a connection string named 'DefaultConnection'");
+            }
+
+            return Create(connstr);
+        }
+
+        private ApplicationContext Create(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException($"{nameof(connectionString)} is null or empty");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationContext(optionsBuilder.Options);
+        }
+
+        public ApplicationContext Create(DbContextFactoryOptions options)
+        {
+            return Create(options.ContentRootPath, options.EnvironmentName);
+        }
+    }
+}
