95d5d733d784c9ab6fcd0c1d4dd4e4df34f345d2
Adrian Limon <me@adrianlimon.com>
2017-07-21 11:30:00 -0700

    add migrations to data layer project

---
+++ ContosoUniversity.Data/Migrations/20170809053749_InitialCreate.Designer.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ContosoUniversity.Data;
+
+namespace ContosoUniversity.Data.Migrations
+{
+    [DbContext(typeof(ApplicationContext))]
+    [Migration("20170809053749_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Course", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("Credits");
+
+                    b.Property<int>("DepartmentID");
+
+                    b.Property<string>("Title")
+                        .HasMaxLength(50);
+
+                    b.HasKey("CourseID");
+
+                    b.HasIndex("DepartmentID");
+
+                    b.ToTable("Course","Contoso");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.CourseAssignment", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("InstructorID");
+
+                    b.HasKey("CourseID", "InstructorID");
+
+                    b.HasIndex("InstructorID");
+
+                    b.ToTable("CourseAssignment","Contoso");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Department", b =>
+                {
+                    b.Property<int>("DepartmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<decimal>("Budget")
+                        .HasColumnType("money");
+
+                    b.Property<int?>("InstructorID");
+
+                    b.Property<string>("Name")
+                        .HasMaxLength(50);
+
+                    b.Property<byte[]>("RowVersion")
+                        .IsConcurrencyToken()
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("DepartmentID");
+
+                    b.HasIndex("InstructorID");
+
+                    b.ToTable("Department","Contoso");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Enrollment", b =>
+                {
+                    b.Property<long>("EnrollmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<long>("CourseID");
+
+                    b.Property<int?>("CourseID1");
+
+                    b.Property<int?>("Grade");
+
+                    b.Property<long>("StudentID");
+
+                    b.Property<int?>("StudentID1");
+
+                    b.HasKey("EnrollmentID");
+
+                    b.HasIndex("CourseID1");
+
+                    b.HasIndex("StudentID1");
+
+                    b.ToTable("Enrollment","Contoso");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.OfficeAssignment", b =>
+                {
+                    b.Property<int>("InstructorID");
+
+                    b.Property<string>("Location")
+                        .HasMaxLength(50);
+
+                    b.HasKey("InstructorID");
+
+                    b.ToTable("OfficeAssignment","Contoso");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Person", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
+                    b.Property<string>("FirstMidName")
+                        .IsRequired()
+                        .HasColumnName("FirstName")
+                        .HasMaxLength(50);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Person","Contoso");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("Person");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Instructor", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Data.Entities.Person");
+
+                    b.Property<DateTime>("HireDate");
+
+                    b.ToTable("Instructor","Contoso");
+
+                    b.HasDiscriminator().HasValue("Instructor");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Student", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Data.Entities.Person");
+
+                    b.Property<DateTime>("EnrollmentDate");
+
+                    b.ToTable("Student","Contoso");
+
+                    b.HasDiscriminator().HasValue("Student");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Course", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Entities.Department", "Department")
+                        .WithMany("Courses")
+                        .HasForeignKey("DepartmentID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.CourseAssignment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Entities.Course", "Course")
+                        .WithMany("CourseAssignments")
+                        .HasForeignKey("CourseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("ContosoUniversity.Data.Entities.Instructor", "Instructor")
+                        .WithMany("CourseAssignments")
+                        .HasForeignKey("InstructorID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Department", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Entities.Instructor", "Administrator")
+                        .WithMany()
+                        .HasForeignKey("InstructorID");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.Enrollment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Entities.Course", "Course")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("CourseID1");
+
+                    b.HasOne("ContosoUniversity.Data.Entities.Student", "Student")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("StudentID1");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Entities.OfficeAssignment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Entities.Instructor", "Instructor")
+                        .WithOne("OfficeAssignment")
+                        .HasForeignKey("ContosoUniversity.Data.Entities.OfficeAssignment", "InstructorID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ ContosoUniversity.Data/Migrations/20170809053749_InitialCreate.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ContosoUniversity.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Contoso");
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Discriminator = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Budget = table.Column<decimal>(type: "money", nullable: false),
+                    InstructorID = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentID);
+                    table.ForeignKey(
+                        name: "FK_Department_Person_InstructorID",
+                        column: x => x.InstructorID,
+                        principalSchema: "Contoso",
+                        principalTable: "Person",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfficeAssignment",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    InstructorID = table.Column<int>(nullable: false),
+                    Location = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficeAssignment", x => x.InstructorID);
+                    table.ForeignKey(
+                        name: "FK_OfficeAssignment_Person_InstructorID",
+                        column: x => x.InstructorID,
+                        principalSchema: "Contoso",
+                        principalTable: "Person",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(nullable: false),
+                    Credits = table.Column<int>(nullable: false),
+                    DepartmentID = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseID);
+                    table.ForeignKey(
+                        name: "FK_Course_Department_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalSchema: "Contoso",
+                        principalTable: "Department",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseAssignment",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(nullable: false),
+                    InstructorID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseAssignment", x => new { x.CourseID, x.InstructorID });
+                    table.ForeignKey(
+                        name: "FK_CourseAssignment_Course_CourseID",
+                        column: x => x.CourseID,
+                        principalSchema: "Contoso",
+                        principalTable: "Course",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseAssignment_Person_InstructorID",
+                        column: x => x.InstructorID,
+                        principalSchema: "Contoso",
+                        principalTable: "Person",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollment",
+                schema: "Contoso",
+                columns: table => new
+                {
+                    EnrollmentID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CourseID = table.Column<long>(nullable: false),
+                    CourseID1 = table.Column<int>(nullable: true),
+                    Grade = table.Column<int>(nullable: true),
+                    StudentID = table.Column<long>(nullable: false),
+                    StudentID1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollment", x => x.EnrollmentID);
+                    table.ForeignKey(
+                        name: "FK_Enrollment_Course_CourseID1",
+                        column: x => x.CourseID1,
+                        principalSchema: "Contoso",
+                        principalTable: "Course",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Enrollment_Person_StudentID1",
+                        column: x => x.StudentID1,
+                        principalSchema: "Contoso",
+                        principalTable: "Person",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_DepartmentID",
+                schema: "Contoso",
+                table: "Course",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseAssignment_InstructorID",
+                schema: "Contoso",
+                table: "CourseAssignment",
+                column: "InstructorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_InstructorID",
+                schema: "Contoso",
+                table: "Department",
+                column: "InstructorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_CourseID1",
+                schema: "Contoso",
+                table: "Enrollment",
+                column: "CourseID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_StudentID1",
+                schema: "Contoso",
+                table: "Enrollment",
+                column: "StudentID1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseAssignment",
+                schema: "Contoso");
+
+            migrationBuilder.DropTable(
+                name: "Enrollment",
+                schema: "Contoso");
+
+            migrationBuilder.DropTable(
+                name: "OfficeAssignment",
+                schema: "Contoso");
+
+            migrationBuilder.DropTable(
+                name: "Course",
+                schema: "Contoso");
+
+            migrationBuilder.DropTable(
+                name: "Department",
+                schema: "Contoso");
+
+            migrationBuilder.DropTable(
+                name: "Person",
+                schema: "Contoso");
+        }
+    }
+}
