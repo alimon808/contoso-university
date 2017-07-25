b5b4c98b60d0425323cbf69219cdc6b684931710
Adrian Limon <me@adrianlimon.com>
2017-07-25 10:38:35 -0700

    model binding helper TryUpdateModelAsync to support unit testing

---
+++ ContosoUniversity.Web/DefaultModelBindingHelperAdaptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace ContosoUniversity.Web
+{
+    // issue with unit testing controllers that use TryUpdateModelAsync
+    // solution @ https://stackoverflow.com/questions/39259025/asp-net-core-mvc-controller-unit-testing-when-using-tryupdatemodel
+    public class DefaultModelBindingHelaperAdaptor : IModelBindingHelperAdaptor
+    {
+        public Task<bool> TryUpdateModelAsync<TModel>(ControllerBase controller, TModel model, string prefix, params Expression<Func<TModel, object>>[] includeExpressions) where TModel : class
+        {
+            return controller.TryUpdateModelAsync(model, prefix, includeExpressions);
+        }
+    }
+}
---
+++ ContosoUniversity.Web/IModelBindingHelperAdaptor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Web
+{
+    public interface IModelBindingHelperAdaptor
+    {
+        Task<bool> TryUpdateModelAsync<TModel>(ControllerBase controller, TModel model, string prefix, params Expression<Func<TModel, object>>[] includeExpressions) where TModel : class;
+    }
+}
