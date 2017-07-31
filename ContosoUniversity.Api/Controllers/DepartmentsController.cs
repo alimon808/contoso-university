cea33da3daaeefb940c592958e5cae0824f06b8d
Adrian Limon <me@adrianlimon.com>
2017-07-31 11:37:51 -0700

    setup initial ContosoUniversity.Api project

---
+++ ContosoUniversity.Api/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ContosoUniversity.Data.Interfaces;
+using ContosoUniversity.Data.Entities;
+using System.Linq;
+
+namespace ContosoUniversity.Api.Controllers
+{
+    [Route("[controller]")]
+    public class DepartmentsController : Controller
+    {
+        private IRepository<Department> _departmentRepo;
+
+        public DepartmentsController(IRepository<Department> departmentRepo)
+        {
+            _departmentRepo = departmentRepo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> GetAll()
+        {
+            return _departmentRepo.GetAll().ToList();
+        }
+    }
+}
---
+++ ContosoUniversity.Api/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Data.Interfaces;
+
+namespace ContosoUniversity.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            CurrentEnvironment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment CurrentEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (CurrentEnvironment.IsEnvironment("Testing"))
+            {
+                services.AddDbContext<ApplicationContext>(optionsBuilder => optionsBuilder.UseInMemoryDatabase());
+            } else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsHistoryTable("Migration", "Contoso"));
+                });
+            }
+            
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, ApplicationContext context)
+        {
+            if (CurrentEnvironment.IsDevelopment())
+            {
+                DbInitializer.Initialize(context);
+            }
+
+            app.UseMvc();
+        }
+    }
+}
