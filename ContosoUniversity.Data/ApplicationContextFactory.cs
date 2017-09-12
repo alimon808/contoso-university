debd1c7c960f8d49df20075322c87e034d18c578
Adrian Limon <me@adrianlimon.com>
2017-09-12 09:31:57 -0700

    migrated ContosoUniversity.Data project to core 2.0

--- ContosoUniversity.Data/ApplicationContextFactory.cs
+++ ContosoUniversity.Data/ApplicationContextFactory.cs
@@ -1,56 +1,80 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 
 namespace ContosoUniversity.Data
 {
     // modeled from https://www.benday.com/2017/02/17/ef-core-migrations-without-hard-coding-a-connection-string-using-idbcontextfactory
-    public class ApplicationContextFactory : IDbContextFactory<ApplicationContext>
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
     {
-        public ApplicationContext Create()
+        public ApplicationContext CreateDbContext(string[] args)
         {
-            var environmentName = Environment.GetEnvironmentVariable("Hosting:Environment");
-            var basePath = AppContext.BaseDirectory;
-            return Create(basePath, environmentName);
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("Hosting:Environment")}.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationContext>();
+            builder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            return new ApplicationContext(builder.Options);
         }
 
-        private ApplicationContext Create(string basePath, string environmentName)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(basePath)
-                .AddJsonFile("settings.json")
-                .AddJsonFile($"settings.{environmentName}.json", true)
-                .AddEnvironmentVariables();
+        //public ApplicationContext Create()
+        //{
+        //    var environmentName = Environment.GetEnvironmentVariable("Hosting:Environment");
+        //    var basePath = AppContext.BaseDirectory;
 
-            var config = builder.Build();
+        //    return Create(basePath, environmentName);
+        //}
 
-            var connstr = config.GetConnectionString("DefaultConnection");
+        //private ApplicationContext Create(string basePath, string environmentName)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(basePath)
+        //        .AddJsonFile("appsettings.json")
+        //        .AddJsonFile($"appsettings.{environmentName}.json", true)
+        //        .AddEnvironmentVariables();
 
-            if (String.IsNullOrWhiteSpace(connstr))
-            {
-                throw new InvalidOperationException("Could not find a connection string named 'DefaultConnection'");
-            }
+        //    var config = builder.Build();
 
-            return Create(connstr);
-        }
+        //    var connstr = config.GetConnectionString("DefaultConnection");
 
-        private ApplicationContext Create(string connectionString)
-        {
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new ArgumentException($"{nameof(connectionString)} is null or empty");
-            }
+        //    if (String.IsNullOrWhiteSpace(connstr))
+        //    {
+        //        throw new InvalidOperationException("Could not find a connection string named 'DefaultConnection'");
+        //    }
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-            optionsBuilder.UseSqlServer(connectionString, x => x.MigrationsHistoryTable("Migration", "Contoso"));
+        //    return Create(connstr);
+        //}
 
-            return new ApplicationContext(optionsBuilder.Options);
-        }
+        //private ApplicationContext Create(string connectionString)
+        //{
+        //    if (string.IsNullOrEmpty(connectionString))
+        //    {
+        //        throw new ArgumentException($"{nameof(connectionString)} is null or empty");
+        //    }
 
-        public ApplicationContext Create(DbContextFactoryOptions options)
-        {
-            return Create(options.ContentRootPath, options.EnvironmentName);
-        }
+        //    var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        //    optionsBuilder.UseSqlServer(connectionString, x => x.MigrationsHistoryTable("Migration", "Contoso"));
+
+        //    return new ApplicationContext(optionsBuilder.Options);
+        //}
+
+        ////public ApplicationContext Create(DbContextFactoryOptions options)
+        ////{
+        ////    return Create(options.ContentRootPath, options.EnvironmentName);
+        ////}
+
+        //public ApplicationContext Create(DbContextFactoryOptions options)
+        //{
+        //    return Create(Directory.GetCurrentDirectory(), Environment.GetEnvironmentVariable("Hosting:Environment"));
+        //}
+
+
     }
 }
