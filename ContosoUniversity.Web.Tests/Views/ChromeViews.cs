f2b8166840dce88e771e941ec575de368bcf4d97
Adrian Limon <me@adrianlimon.com>
2017-10-22 16:22:11 -0700

    add first ui test using selenium + chrome

---
+++ ContosoUniversity.Web.Tests/Views/ChromeViews.cs
@@ -0,0 +1,34 @@
+﻿using ContosoUniversity.Tests;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace ContosoUniversity.Web.Tests.Views
+{
+    public class ChromeViews : BaseIntegrationTest<Startup>
+    {
+        private readonly Uri _baseUrl;
+
+        public ChromeViews()
+        {
+            _baseUrl = new Uri("https://localhost:44368/");
+        }
+
+        [Fact]
+        public void HomeIndex_ShouldHaveAJumbotron_WithH1()
+        {
+            var home = _baseUrl;
+            using (var driver = new ChromeDriver
+                  (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {
+                driver.Navigate().GoToUrl(home);
+                var jumbotron = driver.FindElementByClassName("jumbotron");
+                var h1 = jumbotron.FindElement(By.TagName("h1"));
+                Assert.Equal("Contoso University", h1.Text);
+            }
+        }
+    }
+}
