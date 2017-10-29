67ac0df015c687e06ed190c3758d1d954e697908
Adrian Limon <me@adrianlimon.com>
2017-10-29 11:11:24 -0700

    add ApiPfrofile & Api/DTO

---
+++ ContosoUniversity.Api/ApiProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ContosoUniversity.Api.DTO;
+using ContosoUniversity.Common.DTO;
+using ContosoUniversity.Data.Entities;
+
+namespace ContosoUniversity.Api
+{
+    public class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<DepartmentDTO, Department>().ReverseMap();
+            CreateMap<CreateDepartmentDTO, Department>().ReverseMap();
+        }
+    }
+}
---
+++ ContosoUniversity.Api/DTO/CreateDepartmentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Api.DTO
+{
+    public class CreateDepartmentDTO
+    {
+        [Required]
+        public int InstructorID { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Budget { get; set; } = 0;
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
