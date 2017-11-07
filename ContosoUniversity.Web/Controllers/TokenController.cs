00effb713c99ab3d278ade43fcdedbc2c89a94af
Adrian Limon <me@adrianlimon.com>
2017-11-07 10:40:14 -0800

    add multiple jwt audiences

--- ContosoUniversity.Web/Controllers/TokenController.cs
+++ ContosoUniversity.Web/Controllers/TokenController.cs
@@ -10,11 +10,13 @@ using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace ContosoUniversity.Web.Controllers
 {
     // model after http://www.blinkingcaret.com/2017/09/06/secure-web-api-in-asp-net-core/
-    [Authorize]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     public class TokenController : Controller
     {
@@ -27,6 +29,11 @@ namespace ContosoUniversity.Web.Controllers
             _signInManager = signInManager;
             _userManager = userManager;
             _config = config;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok("Congrats, you're authenticated.");
         }
 
         [HttpPost]
@@ -42,17 +49,24 @@ namespace ContosoUniversity.Web.Controllers
                     var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
                     if (result.Succeeded)
                     {
-                        var claims = new[]
+                        var claims = new List<Claim>
                         {
                             new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
                         };
 
+                        // support multiple audiences
+                        var audiences = _config.GetSection("Authentication:Tokens:Audiences").Get<string[]>();
+                        foreach (var item in audiences)
+                        {
+                            claims.Add(new Claim("aud", item));
+                        }
+
                         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Authentication:Tokens:Key"]));
                         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
                         var token = new JwtSecurityToken(_config["Authentication:Tokens:Issuer"],
-                            _config["Authentication:Tokens:Issuer"],
+                            null,
                             claims,
                             expires: DateTime.Now.AddMinutes(30),
                             signingCredentials: creds);
