4243a235068beb64ef7c134e8b4844a05ba0359a
Adrian Limon <me@adrianlimon.com>
2018-02-15 10:46:13 -0800

    add automapper

---
+++ ContosoUniversity.Spa.React/ReactApiProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ContosoUniversity.Spa.React.DTO;
+using ContosoUniversity.Common.DTO;
+using ContosoUniversity.Data.Entities;
+
+namespace ContosoUniversity.Spa.React
+{
+    public class ReactApiProfile : Profile
+    {
+        public ReactApiProfile()
+        {
+            CreateMap<DepartmentDTO, Department>().ReverseMap();
+        }
+    }
+}
