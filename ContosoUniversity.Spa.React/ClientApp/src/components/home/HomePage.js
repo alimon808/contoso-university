fa7f0475f3cd9f67b4bf69efb0816dc6eead394e
Adrian Limon <me@adrianlimon.com>
2018-05-03 11:51:02 -0700

    update HomePage to mirror web-app content

--- ContosoUniversity.Spa.React/ClientApp/src/components/home/HomePage.js
+++ ContosoUniversity.Spa.React/ClientApp/src/components/home/HomePage.js
@@ -1,23 +1,44 @@
 import React from 'react';
+import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
-export default function() {
-    return(
-        <div>
-        <h1>Hello, students!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-          <li><a href='https://github.com/alimon808/contoso-university/tree/master/ContosoUniversity.Spa.React'>Source Code</a> on Github</li>
-        </ul>
-        <p>To help you get started, we've also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+export default function () {
+    return (
+        <div class="container">
+            <Jumbotron>
+                <h1>Contoso University</h1>
+                <p>Welcome to your new single-page application, built with:</p>
+                <ul>
+                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+                    <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+                    <li><a href='https://github.com/alimon808/contoso-university/tree/master/ContosoUniversity.Spa.React'>Source Code</a> on Github</li>
+                </ul>
+            </Jumbotron>
+            <Grid>
+                <Row>
+                    <Col md={4}>
+                        <p>
+                            Contoso University is a place for learning AspNetCore and related technologies.  This demo application is an amalgamation of smaller demo applications found in tutorials at <a href="https://docs.microsoft.com/en-us/aspnet/core/">AspNetCore docs</a>.  The tutorials are great at demonstrating isolated concepts, but issues surfaces when applying these concepts/techniques in a larger context.  The purpose of this demo application is to apply concepts/techniques learned from those tutorial into a single domain (i.e. university).
+                        </p>
+                    </Col>
+                    <Col md={4}>
+                        <h2>Build it from scratch</h2>
+                        <p>
+                            You can build the initial application by following the steps in a series of tutorials.
+                        </p>
+                        <p>
+                            <a class="btn btn-default" href="https://docs.microsoft.com/en-us/aspnet/core/spa/react?view=aspnetcore-2.1&tabs=visual-studio">Seee the tutorial &raquo;</a>
+                        </p>
+                    </Col>
+                    <Col md={4}>
+                        <h2>Download it</h2>
+                        <p>You can download the completed project from Github.</p>
+                        <p>
+                            <a class="btn btn-default" href="https://github.com/alimon808/contoso-university/tree/master/ContosoUniversity.Spa.React">See project source code &raquo;</a>
+                        </p>
+                    </Col>
+                </Row>
+            </Grid>
+        </div>
     );
 }
