ac295be29bc5aab81ee5d5407821b8148d0a47a6
adrianlimon <me@adrianlimon.com>
2018-05-07 11:11:12 -0700

    add react contactpage content

--- ContosoUniversity.Spa.React/ClientApp/src/components/contact/ContactPage.js
+++ ContosoUniversity.Spa.React/ClientApp/src/components/contact/ContactPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function() {
-    return(
+export default function () {
+    return (
         <div>
-            <h1>Contact Page</h1>
+            <h1>Contact</h1>
+            <address>
+                One Microsoft Way <br />
+                Redmond, WA 98052-6399 <br />
+                <abbr title="Phone">P:</abbr>425.555.0100
+                </address>
+            <address>
+                <strong>Support: </strong><a href="mailto:Support@example.com">Support@example.com</a><br />
+                <strong>Marketing: </strong><a href="mailto:Marketing@example.com">Marketing@example.com</a><br />
+            </address>
         </div>
     );
 }
