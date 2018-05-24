3d022134eee28884c8673fab3cb3b283d4fc7733
Adrian Limon <me@adrianlimon.com>
2018-05-24 14:58:34 -0700

    add confirm password validation

--- ContosoUniversity.Spa.React/ClientApp/src/components/forms/Register.js
+++ ContosoUniversity.Spa.React/ClientApp/src/components/forms/Register.js
@@ -29,9 +29,10 @@ export default class Register extends React.Component {
             email: '',
             password: '',
             confirmPassword: '',
-            formErrors: { email: '', password: '' },
+            formErrors: { email: '', password: '', confirmPassword: '' },
             emailValid: false,
             passwordValid: false,
+            confirmPasswordValid: false,
             formValid: false
         };
         this.handleClick = this.handleClick.bind(this);
@@ -52,16 +53,20 @@ export default class Register extends React.Component {
         let fieldValidationErrors = this.state.formErrors;
         let emailValid = this.state.emailValid;
         let passwordValid = this.state.passwordValid;
+        let confirmPasswordValid = this.state.confirmPasswordValid;
 
         switch (fieldName) {
             case 'email':
                 emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.email = emailValid ? '' : ' is valid';
+                fieldValidationErrors.email = emailValid ? '' : ' is invalid';
                 break;
             case 'password':
                 passwordValid = value.length >= 6;
                 fieldValidationErrors.password = passwordValid ? '' : ' is too short';
                 break;
+            case 'confirmPassword':
+                confirmPasswordValid = this.state.password === value;
+                fieldValidationErrors.confirmPassword = confirmPasswordValid ? '' : ' does not match password';
             default:
                 break;
         }
@@ -69,12 +74,14 @@ export default class Register extends React.Component {
         this.setState({
             formErrors: fieldValidationErrors,
             emailValid: emailValid,
-            passwordValid: passwordValid
+            passwordValid: passwordValid,
+            confirmPasswordValid: confirmPasswordValid
         }, this.validateForm);
     }
 
     validateForm() {
-        this.setState({ formValid: this.state.emailValid });
+        const formValid = this.state.emailValid && this.state.passwordValid && this.state.confirmPasswordValid;
+        this.setState({ formValid: formValid });
     }
 
     validationState(error) {
@@ -99,9 +106,11 @@ export default class Register extends React.Component {
                     <FormControl.Feedback />
                     </Col>
                 </FormGroup>
-                <FormGroup>
+                <FormGroup validationState={this.validationState(this.state.formErrors.confirmPassword)}>
                     <Col componentClass={ControlLabel} sm={2}>Confirm Password</Col>
-                    <Col sm={10}><FormControl type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} /></Col>
+                    <Col sm={10}><FormControl type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} />
+                        <FormControl.Feedback />
+                    </Col>
                 </FormGroup>
                 <FormGroup>
                     <Col componentClass={ControlLabel} sm={2}></Col>
