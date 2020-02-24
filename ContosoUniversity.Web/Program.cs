04f0ebd47431d8debdfa66a5e1acca4285c8dc95
Adrian Limon <me@adrianlimon.com>
2020-02-23 23:32:20 -0800

    Replace BuildWebHost => CreatewebHostBuilder to support integration testing

--- ContosoUniversity.Web/Program.cs
+++ ContosoUniversity.Web/Program.cs
@@ -10,15 +10,16 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) => 
+        // netcoreapp2.1 code-based idiom to support integration test infrastructor
+        // https://docs.microsoft.com/en-us/aspnet/core/migration/20_21?view=aspnetcore-3.1
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(ConfigConfiguration)
                 .ConfigureLogging(ConfigureLogger)
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
 
         // refs: 
         // https://wildermuth.com/2017/07/06/Program-cs-in-ASP-NET-Core-2-0
@@ -26,12 +27,12 @@ namespace ContosoUniversity
         {
             config.SetBasePath(Directory.GetCurrentDirectory());
 
-            if (context.HostingEnvironment.IsDevelopment()) 
+            if (context.HostingEnvironment.IsDevelopment())
             {
                 config.AddJsonFile($"sampleData.json", optional: true, reloadOnChange: false);
                 config.AddUserSecrets<Startup>();
             }
-            
+
             config.AddEnvironmentVariables();
         }
 
